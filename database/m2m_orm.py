d915b7f7da4b08a5565738b17b191af07e79bbd6
andrew <andrewdevandroid@gmail.com>
2021-11-14 10:55:40 +0300

    Remove unique constraints and refactor.

--- database/m2m_orm.py
+++ database/m2m_orm.py
@@ -5,34 +5,37 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
-engine = create_engine("sqlite:///database/db.sqlite3",
-                       encoding='utf-8')
+engine = create_engine(
+    "sqlite:///database/db.sqlite3",
+    encoding='utf-8'
+)
 
 Base = declarative_base()  # Establish orm Base class
 
 # After this table is created, it does not need to be maintained
-store_book_author = Table("store_book_author", Base.metadata,
-                          Column("id", Integer, primary_key=True),
-                          Column('book_id', Integer, ForeignKey("store_book.id")),
-                          Column('author_id', Integer, ForeignKey("store_author.id"))
-                          )
+store_book_author = Table(
+    "store_book_author", Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column('book_id', Integer, ForeignKey("store_book.id")),
+    Column('author_id', Integer, ForeignKey("store_author.id"))
+)
 
 
 class Book(Base):
     __tablename__ = "store_book"
     id = Column(Integer, primary_key=True)
-    title = Column(String(64), unique=True)
+    title = Column(String(64))
     pub_date = Column(DATE)
     authors = relationship("Author", secondary=store_book_author, backref="store_books")
 
     def __repr__(self):
-        return self.name
+        return self.title
 
 
 class Author(Base):
     __tablename__ = "store_author"
     id = Column(Integer, primary_key=True)
-    name = Column(String(32), unique=True)
+    name = Column(String(32))
 
     def __repr__(self):
         return self.name
